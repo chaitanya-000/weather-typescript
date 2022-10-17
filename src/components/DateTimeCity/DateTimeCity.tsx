95a9f09d3f84ca60da20a811d7bf5c7f5f59cee3
chaitanya-000 <chaitanya99j@gmail.com>
2022-10-17 16:50:05 +0530

    checkpoint :  undo when react query messes up

--- src/components/DateTimeCity/DateTimeCity.tsx
+++ src/components/DateTimeCity/DateTimeCity.tsx
@@ -1,54 +1,59 @@
-import React, { useContext } from "react";
-import "./DateTimeCity.css";
-import SearchIcon from "@mui/icons-material/Search";
-import { Context } from "../../RootContainer";
-import { useForm } from "react-hook-form";
+import React, { useContext } from 'react';
+import './DateTimeCity.css';
+import SearchIcon from '@mui/icons-material/Search';
+import { Context } from '../../RootContainer';
+import { useForm } from 'react-hook-form';
 
 type Inputs = {
-  city: string;
+	city: string;
 };
 
 const DateTimeCity: React.FC = () => {
-  const { cityName, setCityName, apiData, setApiData } = useContext(Context);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
+	//context
+	const { cityName, setCityName, apiData, setApiData } = useContext(Context);
 
-  const onSubmit = (data: { city: string }) => {
-    console.log(data);
-    setCityName(data.city);
-  };
+	//react-form-hook library
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>();
 
-  return (
-    <div className="container">
-      <h2 className="container__day">
-        {new Date().toLocaleDateString("default", {
-          weekday: "long",
-          month: "long",
-          day: "numeric",
-        })}{" "}
-        2022
-      </h2>
-      <form
-        action="#"
-        className="container__form"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <SearchIcon className="container__icon" />
-        <input
-          // name="city"
-          type="search"
-          className="container__input"
-          placeholder="Eg. Mumbai...."
-          {...register("city")}
-        />
-      </form>
+	//react-query
 
-      <h1 className="container__cityName">{cityName}</h1>
-    </div>
-  );
+	const onSubmit = (data: { city: string }) => {
+		console.log(data);
+		setCityName(data.city);
+	};
+
+	return (
+		<div className='container'>
+			<h2 className='container__day'>
+				{new Date().toLocaleDateString('default', {
+					weekday: 'long',
+					month: 'long',
+					day: 'numeric',
+				})}{' '}
+				2022
+			</h2>
+			<form
+				action='#'
+				className='container__form'
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<SearchIcon className='container__icon' />
+				<input
+					// name="city"
+					type='search'
+					className='container__input'
+					placeholder='Eg. Mumbai....'
+					{...register('city')}
+				/>
+			</form>
+
+			<h1 className='container__cityName'>{cityName}</h1>
+		</div>
+	);
 };
 
 export default DateTimeCity;
