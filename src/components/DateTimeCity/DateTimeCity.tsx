bfcd959051423b26c245c450156acb19e863db1f
chaitanya-000 <chaitanya99j@gmail.com>
2022-10-17 12:57:30 +0530

    react-form-hooks used and working fine

--- src/components/DateTimeCity/DateTimeCity.tsx
+++ src/components/DateTimeCity/DateTimeCity.tsx
@@ -1,17 +1,24 @@
-import React, { useState, useContext, createContext } from "react";
+import React, { useContext } from "react";
 import "./DateTimeCity.css";
 import SearchIcon from "@mui/icons-material/Search";
 import { Context } from "../../RootContainer";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  city: string;
+};
 
 const DateTimeCity: React.FC = () => {
   const { cityName, setCityName, apiData, setApiData } = useContext(Context);
-  const cityRef = React.useRef<HTMLInputElement>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const inputCityName = cityRef?.current?.value;
-    setCityName(inputCityName);
-    console.log("This is the submit button");
+  const onSubmit = (data: { city: string }) => {
+    console.log(data);
+    setCityName(data.city);
   };
 
   return (
@@ -24,14 +31,18 @@ const DateTimeCity: React.FC = () => {
         })}{" "}
         2022
       </h2>
-      <form action="#" className="container__form" onSubmit={handleSubmit}>
+      <form
+        action="#"
+        className="container__form"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <SearchIcon className="container__icon" />
         <input
-          ref={cityRef}
-          name="city"
+          // name="city"
           type="search"
           className="container__input"
-          placeholder="Search weather for..."
+          placeholder="Eg. Mumbai...."
+          {...register("city")}
         />
       </form>
 
